cb868e23ccc3b5b8c12d5aef779e9c1dbf64abb7
Jack Lu <jacklu@clanent.net>
2013-03-10 23:26:49 -0400

    Fixed problem with W3MMD SetWinner.
    Turns out that W3MMD PID is actually the player color, not the player id!

--- game.cpp
+++ game.cpp
@@ -3036,6 +3036,7 @@ bool CGame :: EventPlayerBotCommand( CGamePlayer *player, string command, string
 				SendAllChat( "The game has now been recorded as a draw. You may leave at any time." );
 				m_Stats->LockStats( );
 				m_SoftGameOver = true;
+				m_Stats->LockStats( );
 			}
 			else if( ChangedVote ) //only display message if they actually changed vote
 			{
